36cdff8013ae7250112b36d5d530b73787645c5c
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2024-06-22 17:34:21 +0700

    [sort]: bubble sort

---
+++ sort/bubble_sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ARRAY_LENGTH 100
+
+void swap(int* a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubble_sort(int *array, int size, int *sorted)
+{
+    memcpy(sorted, array, size * sizeof(int));
+    for (;;)
+    {
+        int number_of_shuffles = 0;
+        for (int j = 0; j < size - 1; ++j)
+        {
+            if (sorted[j] > sorted[j + 1])
+            {
+                swap(sorted + j, sorted + j + 1);
+                ++number_of_shuffles;
+            }
+        }
+        if (!number_of_shuffles)
+        {
+            return;
+        }
+    }
+}
+
+void sort_and_print(int *array, int size)
+{
+    int sorted[size];
+    bubble_sort(array, size, sorted);
+
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("-> ");
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d ", sorted[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    const int number_of_arrays = 4;
+    int arrays[][MAX_ARRAY_LENGTH] = {{}, {1}, {6, 7, 2, 1, -1}, {4, 3, 1, -1, 5, 3, 1, 100, 1, 10}};
+    int sizes[] = {0, 1, 5, 10};
+
+    for (int i = 0; i < number_of_arrays; ++i)
+    {
+        sort_and_print(arrays[i], sizes[i]);
+    }
+
+    return 0;
+}
