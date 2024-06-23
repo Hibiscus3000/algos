3ae84d8995bbc0fd2baf4766eee8984e9edd4632
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2024-06-23 20:56:12 +0700

    [numbers]:  gcd

---
+++ numbers/gcd.c
@@ -0,0 +1,59 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+long long str_to_ll(char *str)
+{
+    long long result = strtoll(str, NULL, 10);
+    if (errno)
+    {
+        perror("strtoll");
+        abort();
+    }
+    return result;
+}
+
+long long llabs(long long x)
+{
+    return x < 0 ? -x : x;
+}
+
+long long eu_mod(long long dividend, long long divider)
+{
+    assert(divider);
+    long long result = dividend % divider;
+    if (result < 0)
+    {
+        result += llabs(divider);
+    }
+    return result;
+}
+
+long long gcd(long long x, long long y)
+{
+    if (0 == x && y != 0)
+    {
+        return y;
+    }
+    if (y == 0)
+    {
+        return x;
+    }
+    int result = eu_mod(x, y);
+    return gcd(y, result);
+}
+
+int main(int argc, char *argv[])
+{
+    if (3 != argc)
+    {
+        fprintf(stderr, "Usage: gcd <first number> <second number>\nResult: greatest common divisor of first and second numbers\n");
+        abort();
+    }
+    long long x = str_to_ll(argv[1]);
+    long long y = str_to_ll(argv[2]);
+
+    printf("%lld\n", gcd(x, y));
+    return 0;
+}
